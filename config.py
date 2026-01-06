0cc7ca8a5ba41e11f49eb475de3912b07d250071
josdic1 <joshuadicker@gmail.com>
2026-01-06 13:55:29 -0500

    update cookie policiy

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = True
